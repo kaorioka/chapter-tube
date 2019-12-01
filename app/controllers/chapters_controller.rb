c3eb8e8693050993081a05d29a4a070d3c083393
Kaori Oka <kaorioka09jm@gmail.com>
2019-12-01 19:29:56 +0900

    waste cord delete

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -1,14 +1,8 @@
 class ChaptersController < ApplicationController
   
-  before_action :set_movie
 
   def show
     @chapter = Chapter.find(params[:id])
-    # @chapters = Movie.chapters.find(params[:movie_id])
-  end
-
-  def set_movie
-    @movie = Movie.find(params[:id])
   end
 
 end
