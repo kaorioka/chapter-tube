26d055603d95a5cea8e849628af36e55f0512787
Kaori Oka <kaorioka09jm@gmail.com>
2019-12-01 17:13:16 +0900

    add Time.at method

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -1,7 +1,14 @@
 class ChaptersController < ApplicationController
   
+  before_action :set_movie
+
   def show
     @chapter = Chapter.find(params[:id])
+    # @chapters = Movie.chapters.find(params[:movie_id])
+  end
+
+  def set_movie
+    @movie = Movie.find(params[:id])
   end
 
 end
