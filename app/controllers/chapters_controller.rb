37030e37182589fae85d3d344b2c757bbcef133d
Kaori Oka <kaorioka09jm@gmail.com>
2019-12-02 02:04:27 +0900

    add @chapters

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -1,8 +1,11 @@
 class ChaptersController < ApplicationController
   
+  def index
+  end
 
   def show
     @chapter = Chapter.find(params[:id])
+    @chapters = Chapter.find(params[:movie_id])
   end
 
 end
