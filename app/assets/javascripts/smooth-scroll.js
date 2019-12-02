7c9db46e28fc7e9bb7094d9ba2a245b7c2527f50
Kaori Oka <kaorioka09jm@gmail.com>
2019-12-03 01:11:34 +0900

    smooth scroll

--- app/assets/javascripts/smooth-scroll.js
+++ app/assets/javascripts/smooth-scroll.js
@@ -1,5 +1,18 @@
 $(function () {
 
+  // smooth scroll anchor_link
+  $('a[href^="#"]').click(function () {
+    var speed = 500;
+    var headerHight = 55; //ヘッダの高さ
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top - headerHight; //ヘッダの高さ分位置をずらす
+    $("html, body").animate({ scrollTop: position }, speed, "swing");
+    return false;
+  });
+
+
+  // top scroll btn
   var topBtn = $('#page_top');
   //ボタンを非表示にする
   topBtn.hide();
