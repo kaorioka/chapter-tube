5985ce446c034cd15f834e5ede0a60384403bcea
Kaori Oka <kaorioka09jm@gmail.com>
2019-12-03 13:52:45 +0900

    add current

--- app/assets/javascripts/chapter.js
+++ app/assets/javascripts/chapter.js
@@ -3,9 +3,10 @@ $(function () {
   var href = $(".movie--info-box__title").data("chapter-id")
   $(".movie--chapter").each(function (i, ele) {
     if ($(ele).data("chapter-id") === href) {
-      $(this).addClass("activ")
+      $(this).addClass("activ");
+      $(".activ .play_chapter").addClass("fa-play");
     } else {
-      $(this).removeClass("activ")
+      $(this).removeClass("activ");
     }
   });
 
