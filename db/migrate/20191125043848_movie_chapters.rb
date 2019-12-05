928dca3045d4c3c9767ecd4efd30150009e90b63
Kaori Oka <kaorioka09jm@gmail.com>
2019-12-05 22:34:51 +0900

    migrate reset

--- db/migrate/20191125043848_movie_chapters.rb
+++
@@ -1,5 +0,0 @@
-class MovieChapters < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :movie_chapters
-  end
-end
--- db/migrate/20191126112211_change_data_name_to_movie.rb
+++
@@ -1,5 +0,0 @@
-class ChangeDataNameToMovie < ActiveRecord::Migration[5.2]
-  def change
-    change_column :movies, :name, :text
-  end
-end
