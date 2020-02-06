75dba489ef2cead65a8f94c29fd3c667abd52da6
Kaori Oka <kaorioka09jm@gmail.com>
2020-02-06 14:11:01 +0900

    add ssociation

--- app/models/chapter.rb
+++ app/models/chapter.rb
@@ -1,7 +1,10 @@
 class Chapter < ApplicationRecord
   belongs_to :movie
+  has_many :favorites
+  has_many :chapters, through: :favorites
   validates :title, presence: true
   validates :kana, presence: true
   validates :start_time, presence: true
   validates :end_time, presence: true
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :favorite
+  has_many :favorites
+  has_many :users, through: :favorites
 end
