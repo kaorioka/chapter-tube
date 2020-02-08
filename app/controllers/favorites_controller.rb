e5cdb28796e1add7dcd9ce70f48c8d1d92edf1d0
Kaori Oka <kaorioka09jm@gmail.com>
2020-02-08 13:32:42 +0900

    gets favo list

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,12 +1,14 @@
 class FavoritesController < ApplicationController
 
   def index
-    @favorites = Favorite.where(user_id: current_user)
+    @chapters = Chapter.where(id: chapter_id = Favorite.where(user_id: current_user.id).select(:chapter_id))
 
-    @chapters = []
-    @favorites.each do |f|
-        @chapters << Chapter.where(id: f.chapter_id)
-    end
+    # @favorites = Favorite.where(user_id: current_user)
+
+    # @chapters = []
+    # @favorites.each do |f|
+    #     @chapters << Chapter.where(id: f.chapter_id)
+    # end
   end
 
 end 
