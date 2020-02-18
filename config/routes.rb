ee4a4d852868b1b76f91d677620ca0700a114ccf
Kaori Oka <kaorioka09jm@gmail.com>
2020-02-18 23:44:28 +0900

    add comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  get 'comments/create'
+  get 'comments/destroy'
+  get 'comments/update'
   devise_for :users 
   get 'users/index'
   get 'favorites/index'
   get 'seaches/index'
   resources :movies do
-    resources :chapters
+    resources :chapters do
+      resources :comments, only: [:create, :destroy, :update]
+    end
   end
   root to: "dashboard#index"
 
