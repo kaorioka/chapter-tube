30cf6ac0acbdfd2dca745d0531dcb6cc606c25a6
Kaori Oka <kaorioka09jm@gmail.com>
2020-02-20 19:35:38 +0900

    anchor

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
   def create
     comment = Comment.create(text: comment_params[:text], user_id: current_user.id, chapter_id: comment_params[:chapter_id])
-    redirect_to movie_chapter_path(comment.chapter.movie.id,comment.chapter.id)
+    redirect_to movie_chapter_path(comment.chapter.movie.id,comment.chapter.id) + "#comment"
   end
 
   def destroy
