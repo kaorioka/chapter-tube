d28719bb05321dfa098f90a6a672440de96ddbdd
Kaori Oka <kaorioka09jm@gmail.com>
2020-03-04 01:06:14 +0900

    edit typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,10 +10,10 @@ class User < ApplicationRecord
   has_many :likes
 
   def already_favorited?(chapter)
-    self.favorites.exits?(chapter_id: chapter.id)
+    self.favorites.exists?(chapter_id: chapter.id)
   end
 
   def already_liked?(chapter)
-    self.likes.exits?(chapter_id: chapter.id)
+    self.likes.exists?(chapter_id: chapter.id)
   end
 end
