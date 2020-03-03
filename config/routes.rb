400f80a4a71b3ec133f28cac0f9db24ac7c842ca
Kaori Oka <kaorioka09jm@gmail.com>
2020-03-03 13:49:11 +0900

    min edit

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,11 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: "dashboard#index"
-  get 'users/index'
-  get 'favorites/index'
-  get 'seaches/index'
-  resources :movies do
+  resources :seaches, only:[:index]
+  resources :movies, only: [:index, :show] do
     resources :chapters do
       resources :comments, only: [:create, :destroy, :update]
     end
   end
   resources :favorites, only: [:index, :create]
-  resources :movies, only: [:index, :show]
 end
