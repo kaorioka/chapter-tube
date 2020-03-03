6fde9c61b8775e4e4e742a0bbddc9482cbdc47a8
Kaori Oka <kaorioka09jm@gmail.com>
2020-03-04 00:25:55 +0900

    add action like#create

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,5 +3,12 @@ class LikesController < ApplicationController
   end
 
   def create
+    Like.create(chapter_id: like_params[:chapter_id], user_id: current_user.id)
+    redirect_to movies_path
+  end
+
+  private
+  def like_params
+    params.permit(:chapter_id)
   end
 end
